90c4d443312fdb4d07a3a3cbb891abef66fd605a
ullah <sheikholeei97@gmail.ocm>
2022-09-15 15:35:36 +0600

    carousel slider

---
+++ js/slider.js
@@ -0,0 +1,25 @@
+const images = [
+    'images/pic-1.jpg',
+    'images/pic-2.jpg',
+    'images/pic-3.jpg',
+    'images/pic-4.jpg',
+    'images/pic-5.jpg',
+    'images/pic-6.jpg',
+    'images/pic-7.jpg',
+    'images/pic-8.jpg',
+    'images/pic-9.jpg',
+];
+
+let imgIndex = 0;
+
+const img = document.getElementById('carousel-img');
+
+const ser = setInterval(()=>{
+    if(imgIndex === images.length){
+        imgIndex = 0;
+    }
+    const imgLink = images[imgIndex];
+    console.log(imgLink);
+    img.setAttribute('src', imgLink);
+    imgIndex++
+},1000);
